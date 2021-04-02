fee44ff8997a52a4fd0b539b1b29a69d2547ad71
FranRom <maildefranz@gmail.com>
2021-04-02 20:55:50 +0200

    feat: Update logoRound

--- src/widgets/Menu/components/CakePrice.tsx
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Skeleton from '../../../components/Skeleton/Skeleton';
 import { LogoRoundIcon } from '../../../components/Svg';
-
-// import Text from '../../../components/Text/Text';
+import Text from '../../../components/Text/Text';
 
 interface Props {
   cakePriceUsd?: number;
@@ -22,19 +22,23 @@ const PriceLink = styled.a`
   }
 `;
 
+// TODO Add proper Balle Price receiving it as a prop
+const cakePriceUsd = 'Price';
+
 const CakePrice: React.FC<Props> = () => {
-  return (
-    // return cakePriceUsd ? (
+  return cakePriceUsd ? (
     <PriceLink
       href="https://bscscan.com/tx/0xa93afa8423564b93e6ce7e61e0c236e128a9bcf1b1d9e139ccfed5c04e3f82c7"
       target="_blank"
     >
       <LogoRoundIcon width="24px" mr="8px" />
       {/* <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text> */}
+      <Text color="textSubtle" bold>
+        {cakePriceUsd}
+      </Text>
     </PriceLink>
-    // ) : (
-    //   <Skeleton width={80} height={24} />
-    // );
+  ) : (
+    <Skeleton width={80} height={24} />
   );
 };
 
