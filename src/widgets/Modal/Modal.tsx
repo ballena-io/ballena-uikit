8425ec99fa5b7cb8aee186ec6f6edc6932887bfd
FranRom <maildefranz@gmail.com>
2021-04-05 17:53:24 +0200

    refactor: Update Modal styles

--- src/widgets/Modal/Modal.tsx
+++ src/widgets/Modal/Modal.tsx
@@ -17,7 +17,7 @@ interface Props extends InjectedProps {
 const StyledModal = styled.div`
   background: ${({ theme }) => theme.modal.background};
   box-shadow: 0px 20px 36px -8px rgba(14, 14, 44, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.05);
-  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 8px;
   width: 100%;
   z-index: ${({ theme }) => theme.zIndices.modal};
@@ -32,7 +32,7 @@ const StyledModal = styled.div`
 const ModalHeader = styled.div`
   display: flex;
   align-items: center;
-  border-bottom: 1px solid #e9eaeb;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
   align-items: center;
   padding: 12px 24px;
 `;
