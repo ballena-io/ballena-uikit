ab1fb2646957d37c50c2257189a6da67f17d0a2c
FranRom <maildefranz@gmail.com>
2021-04-06 20:36:51 +0200

    refactor: Remove unneeded code

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -4,91 +4,11 @@ export const links = [
     icon: 'HomeIcon',
     href: '/',
   },
-  // {
-  //   label: 'Trade',
-  //   icon: 'TradeIcon',
-  //   items: [
-  //     {
-  //       label: 'Exchange',
-  //       href: 'https://exchange.pancakeswap.finance',
-  //     },
-  //     {
-  //       label: 'Liquidity',
-  //       href: 'https://exchange.pancakeswap.finance/#/pool',
-  //     },
-  //   ],
-  // },
   {
     label: 'Vaults',
     icon: 'VaultIcon',
     href: '/farms',
   },
-  // {
-  //   label: 'Pools',
-  //   icon: 'PoolIcon',
-  //   href: '/syrup',
-  // },
-  // {
-  //   label: 'Lottery',
-  //   icon: 'TicketIcon',
-  //   href: '/lottery',
-  // },
-  // {
-  //   label: 'NFT',
-  //   icon: 'NftIcon',
-  //   href: '/nft',
-  // },
-  // {
-  //   label: 'Profile & Teams',
-  //   icon: 'GroupsIcon',
-  //   items: [
-  //     {
-  //       label: 'Leaderboard',
-  //       href: '/teams',
-  //     },
-  //     {
-  //       label: 'YourProfile',
-  //       href: '/',
-  //     },
-  //   ],
-  //   calloutClass: 'rainbow',
-  // },
-  // {
-  //   label: 'Info',
-  //   icon: 'InfoIcon',
-  //   items: [
-  //     {
-  //       label: 'Overview',
-  //       href: 'https://pancakeswap.info',
-  //     },
-  //     {
-  //       label: 'Tokens',
-  //       href: 'https://pancakeswap.info/tokens',
-  //     },
-  //     {
-  //       label: 'Pairs',
-  //       href: 'https://pancakeswap.info/pairs',
-  //     },
-  //     {
-  //       label: 'Accounts',
-  //       href: 'https://pancakeswap.info/accounts',
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: 'IFO',
-  //   icon: 'IfoIcon',
-  //   items: [
-  //     {
-  //       label: 'Next',
-  //       href: '/ifo',
-  //     },
-  //     {
-  //       label: 'History',
-  //       href: '/ifo/history',
-  //     },
-  //   ],
-  // },
   {
     label: 'More',
     icon: 'MoreIcon',
