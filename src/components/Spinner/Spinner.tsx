b7fdefcc6eb136f146aaee5b42925e36e26cb753
FranRom <maildefranz@gmail.com>
2021-04-07 23:13:39 +0200

    feat: Add ballena spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import PanIcon from './PanIcon';
-import PancakeIcon from './PancakeIcon';
-import { SpinnerProps } from './types';
 
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
+import CoinIcon from './CoinIcon';
+import { SpinnerProps } from './types';
 
 const float = keyframes`
 	0% {
-		transform: translatey(0px);
-	}
-	50% {
 		transform: translatey(10px);
 	}
-	100% {
+	50% {
 		transform: translatey(0px);
+	}
+	100% {
+		transform: translatey(10px);
 	}
 `;
 
@@ -29,24 +20,15 @@ const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  animation: ${rotate} 2s linear infinite;
+const FloatingCoinIcon = styled(CoinIcon)`
+  animation: ${float} 1s ease-in-out infinite;
   transform: translate3d(0, 0, 0);
 `;
 
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
-const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
+const Spinner: React.FC<SpinnerProps> = ({ size = 82 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <FloatingCoinIcon width={`${size}px`} />
     </Container>
   );
 };
