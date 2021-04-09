e0d6d622c98cb35c0f12de1b71c3801a6a54e4cc
FranRom <maildefranz@gmail.com>
2021-04-09 20:17:19 +0200

    test: Update snapshots

--- src/__tests__/widgets/menu.test.tsx
+++ src/__tests__/widgets/menu.test.tsx
@@ -46,21 +46,21 @@ it('renders correctly', () => {
   expect(asFragment()).toMatchInlineSnapshot(`
     <DocumentFragment>
       <div
-        class="sc-kLgntA hmuPEi"
+        class="sc-bYEvPH cNGsRF"
       >
         <nav
-          class="sc-iktFzd grEdDx"
+          class="sc-kLgntA dpNHCE"
         >
           <div
             class="sc-bdfBwQ sc-gsTCUz iwJkGQ ckYhbt"
           >
             <button
               aria-label="Toggle menu"
-              class="sc-gKsewC ZqGQC sc-iBPRYJ gJmiPM"
+              class="sc-pFZIQ bbzmsw sc-jrAGrp feWWVW"
               scale="md"
             >
               <svg
-                class="sc-hKgILt kEuOWF"
+                class="sc-gKsewC kIBFDq"
                 color="textSubtle"
                 viewBox="0 0 24 24"
                 width="24px"
@@ -73,11 +73,11 @@ it('renders correctly', () => {
             </button>
             <a
               aria-label="ballena.io home page"
-              class="sc-fubCfw hNSRmn"
+              class="sc-kEjbxe hTKwD"
               href="/"
             >
               <svg
-                class="sc-hKgILt jamBdP mobile-icon"
+                class="sc-gKsewC iGUOBs mobile-icon"
                 color="text"
                 viewBox="0 0 30 26"
                 width="20px"
@@ -89,7 +89,7 @@ it('renders correctly', () => {
                 />
               </svg>
               <svg
-                class="sc-hKgILt jamBdP desktop-icon"
+                class="sc-gKsewC iGUOBs desktop-icon"
                 color="text"
                 viewBox="0 0 116 26"
                 width="20px"
@@ -103,11 +103,61 @@ it('renders correctly', () => {
             </a>
           </div>
           <div
-            class="sc-bdfBwQ sc-gsTCUz iwJkGQ ckYhbt"
+            class="sc-bdfBwQ sc-gsTCUz iwJkGQ cShFkN"
           >
+            <a
+              class="sc-fubCfw kAsmGF"
+              href="https://bscscan.com/tx/0xa93afa8423564b93e6ce7e61e0c236e128a9bcf1b1d9e139ccfed5c04e3f82c7"
+              target="_blank"
+            >
+              <svg
+                class="sc-gKsewC bBefuS"
+                color="text"
+                viewBox="0 0 26 26"
+                width="24px"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="13"
+                  cy="13"
+                  fill="#ebf8ff"
+                  r="12.57"
+                />
+                <path
+                  d="M13,26A13,13,0,1,1,26,13,13,13,0,0,1,13,26ZM13,.85A12.15,12.15,0,1,0,25.15,13,12.16,12.16,0,0,0,13,.85Z"
+                  fill="#00b5eb"
+                />
+                <path
+                  d="M20.78,21.26a13.73,13.73,0,0,0-2-.14,16.29,16.29,0,0,0-2.13.14l-.18,0-.18,0-.18,0h0l-.18,0-.19,0-.13,0h-.05l-.19,0-.18,0-.19,0-.19,0-.19,0-.19,0-.19,0h0l-.14,0h0l-.09,0h-.4l-.08,0-.19,0-.19,0h0l-.19,0-.19,0-.19,0-.19,0-.18,0L12,22l-.19,0-.17,0-.18,0H9.35a9.71,9.71,0,0,1-2.76-.61l-.31-.13a10.23,10.23,0,0,0,1.19.85h0l.29.17h0a9.18,9.18,0,0,0,3.82.82A14.1,14.1,0,0,0,13.75,23h0l1.12-.28c.52-.12,1.06-.25,1.6-.36a16.41,16.41,0,0,1,4.43-.28c.24-.21.48-.43.7-.66h0A7.44,7.44,0,0,0,20.78,21.26Z"
+                  fill="#00b5eb"
+                />
+                <path
+                  d="M16.92,24.39a12.19,12.19,0,0,1-10.12-1l.21,0,1.22.16,1.47.24,1.36.23,1.21.2.85.11.88.09c.44,0,.89.05,1.33.06l1,0Z"
+                  fill="#00b5eb"
+                />
+                <path
+                  d="M20.52,22.42a13,13,0,0,1-1.71,1.13c-.71.15-1.42.29-2.14.38l-.26,0a12.8,12.8,0,0,1-1.52,0c-.48,0-1,0-1.43-.07h0l-.17,0c-.36,0-.72-.07-1.08-.13a4.72,4.72,0,0,1-.65-.15l-.15-.05h0l.44,0a4,4,0,0,0,.6,0,12.5,12.5,0,0,0,1.84-.23c.7-.13,1.39-.3,2.08-.5a12.62,12.62,0,0,1,1.75-.36,12.44,12.44,0,0,1,2.2-.12Z"
+                  fill="#00b5eb"
+                />
+                <path
+                  d="M9.06,23.38l-.26,0L8,23.2l-.4-.08C7.27,23.06,7,23,6.7,23l-.57-.06a12.39,12.39,0,0,1-1.81-1.51l.32,0a3.78,3.78,0,0,1,.88.22c.17.06.33.13.48.2l.48.22.19.1a11,11,0,0,0,1.27.68l1,.44Z"
+                  fill="#00b5eb"
+                />
+                <path
+                  d="M17.45,19.44a4.78,4.78,0,0,1-.33,1.39.16.16,0,0,1-.16.11c-.42,0-.84.1-1.26.16l-1.06.19h0a2.25,2.25,0,0,1-.49.09c-.22.05-.44.08-.66.12,0,0,0,0,0,0l-.41.07s0,0,0,0h0l-.12,0h0a0,0,0,0,1-.05,0h0l-.14,0h0l-.13,0h0a0,0,0,0,1,0,0h0c-.44.07-.87.12-1.31.15s-.75,0-1.12,0H10s0-.06,0-.08l.6-.68a9.31,9.31,0,0,0,1.18-1.8A16.3,16.3,0,0,0,13,16.49l0-.12c.14-.44.26-.87.36-1.31a10.45,10.45,0,0,0,.24-1.57.74.74,0,0,0,0-.26c-.23-.62-1.41-.51-1.92-.45-.76.08-1.5.31-2.26.44a6.35,6.35,0,0,1-3.67-.54,3.84,3.84,0,0,1-1-.66,2.62,2.62,0,0,1-.5-.65l0-.06H4.3a3.82,3.82,0,0,0,.57.11,3.16,3.16,0,0,0,1.52-.27,8.81,8.81,0,0,0,1-.49C7.91,10.35,8.42,10,9,9.71h.05a1.92,1.92,0,0,1,.33-.14A3.63,3.63,0,0,1,10,9.35a3.68,3.68,0,0,1,.87-.09,4.32,4.32,0,0,1,1.19.16l.33.11.09,0a.09.09,0,0,0,0-.05,1.09,1.09,0,0,1,0-.36c0-.13,0-.25,0-.38a4.58,4.58,0,0,1,.3-1.45,6.17,6.17,0,0,1,.63-1.19c.25-.37.55-.7.79-1.08a3.37,3.37,0,0,0,.19-.32,3.4,3.4,0,0,0,.38-1.11A14.53,14.53,0,0,0,14.89,2s0-.06,0-.09a.15.15,0,0,1,.08,0l.3.15a2.08,2.08,0,0,1,1,.94A4,4,0,0,1,16.6,4a5.82,5.82,0,0,1,.13,1.17,9.92,9.92,0,0,1-.34,2.6,6.22,6.22,0,0,1-.56,1.39,4.67,4.67,0,0,0-.65,3.06,12.94,12.94,0,0,0,1.21,2.65l.24.56c.19.47.37.95.51,1.43a9.15,9.15,0,0,1,.29,1.36A5.9,5.9,0,0,1,17.45,19.44Z"
+                  fill="#1d3149"
+                />
+              </svg>
+              <div
+                class="sc-iBPRYJ bcFJuZ"
+                color="textSubtle"
+              >
+                Price
+              </div>
+            </a>
             <div>
               <button
-                class="sc-gKsewC jGFjHM"
+                class="sc-pFZIQ eSzTXS"
                 scale="sm"
               >
                 0xbd...c980
@@ -116,16 +166,16 @@ it('renders correctly', () => {
           </div>
         </nav>
         <div
-          class="sc-jJEJSO hYddnZ"
+          class="sc-iktFzd dABFdo"
         >
           <div
-            class="sc-fKFyDc kASSBr"
+            class="sc-kfzAmx kRrOVp"
           >
             <div
-              class="sc-crrsfI laFgBV"
+              class="sc-kstrdz bYqKmK"
             >
               <div
-                class="sc-jrAGrp bdWccI"
+                class="sc-crrsfI hhvfZD"
                 role="button"
               >
                 <a
@@ -134,7 +184,7 @@ it('renders correctly', () => {
                   href="/"
                 >
                   <svg
-                    class="sc-hKgILt jVEZqh"
+                    class="sc-gKsewC bBefuS"
                     color="text"
                     viewBox="0 0 24 24"
                     width="24px"
@@ -161,21 +211,21 @@ it('renders correctly', () => {
                     />
                   </svg>
                   <div
-                    class="sc-pFZIQ cyJFUw"
+                    class="sc-iqHYGH hEViZq"
                   >
                     Home
                   </div>
                 </a>
               </div>
               <div
-                class="sc-jrAGrp iOgCIp"
+                class="sc-crrsfI dQwqIm"
                 role="button"
               >
                 <a
                   href="/farms"
                 >
                   <svg
-                    class="sc-hKgILt jVEZqh"
+                    class="sc-gKsewC bBefuS"
                     color="text"
                     viewBox="0 0 24 24"
                     width="24px"
@@ -213,21 +263,21 @@ it('renders correctly', () => {
                     />
                   </svg>
                   <div
-                    class="sc-pFZIQ cyJFUw"
+                    class="sc-iqHYGH hEViZq"
                   >
                     Vaults
                   </div>
                 </a>
               </div>
               <div
-                class="sc-kEjbxe gTxQxW"
+                class="sc-dQppl kVjqMM"
               >
                 <div
-                  class="sc-jrAGrp iOgCIp"
+                  class="sc-crrsfI dQwqIm"
                   role="button"
                 >
                   <svg
-                    class="sc-hKgILt jVEZqh"
+                    class="sc-gKsewC bBefuS"
                     color="text"
                     viewBox="0 0 24 24"
                     width="24px"
@@ -238,12 +288,12 @@ it('renders correctly', () => {
                     />
                   </svg>
                   <div
-                    class="sc-pFZIQ cyJFUw"
+                    class="sc-iqHYGH hEViZq"
                   >
                     More
                   </div>
                   <svg
-                    class="sc-hKgILt jamBdP"
+                    class="sc-gKsewC iGUOBs"
                     color="text"
                     viewBox="0 0 24 24"
                     width="20px"
@@ -255,19 +305,19 @@ it('renders correctly', () => {
                   </svg>
                 </div>
                 <div
-                  class="sc-iqHYGH gzGyYh"
+                  class="sc-bqyKva jJwFJZ"
                 />
               </div>
             </div>
             <div
-              class="sc-hHftDr hLoBfO"
+              class="sc-dIUggk hajyYa"
             >
               <button
-                class="sc-gKsewC jxrIVv sc-dQppl bFyzuT"
+                class="sc-pFZIQ daSZot sc-hBEYos hvfepY"
                 scale="md"
               >
                 <svg
-                  class="sc-hKgILt jamBdP"
+                  class="sc-gKsewC iGUOBs"
                   color="text"
                   viewBox="0 0 24 24"
                   width="20px"
@@ -281,12 +331,12 @@ it('renders correctly', () => {
             </div>
           </div>
           <div
-            class="sc-hiSbYr kylPqV"
+            class="sc-jJEJSO eEyHJ"
           >
             body
           </div>
           <div
-            class="sc-dlfnbm sc-gWHgXt kgrCKx IZmxf"
+            class="sc-dlfnbm sc-hiSbYr kgrCKx klxusr"
             role="presentation"
           />
         </div>
