0171afbf85cb54e9f27d76e37ec684a0d20bd2c2
FranRom <maildefranz@gmail.com>
2021-04-11 19:12:58 +0200

    feat: Add external links to menu items

--- src/widgets/Menu/components/PanelBody.tsx
+++ src/widgets/Menu/components/PanelBody.tsx
@@ -54,7 +54,9 @@ const PanelBody: React.FC<Props> = ({ isPushed, pushNav, isMobile, links }) => {
               {isPushed &&
                 entry.items.map((item) => (
                   <MenuEntry key={item.href} secondary isActive={item.href === location.pathname} onClick={handleClick}>
-                    <MenuLink href={item.href}>{item.label}</MenuLink>
+                    <MenuLink href={item.href} target={item.external ? '_blank' : '_self'}>
+                      {item.label}
+                    </MenuLink>
                   </MenuEntry>
                 ))}
             </Accordion>
@@ -62,7 +64,7 @@ const PanelBody: React.FC<Props> = ({ isPushed, pushNav, isMobile, links }) => {
         }
         return (
           <MenuEntry key={entry.label} isActive={entry.href === location.pathname} className={calloutClass}>
-            <MenuLink href={entry.href} onClick={handleClick}>
+            <MenuLink href={entry.href} onClick={handleClick} target={entry.external ? '_blank' : '_self'}>
               {iconElement}
               <LinkLabel isPushed={isPushed}>{entry.label}</LinkLabel>
             </MenuLink>
