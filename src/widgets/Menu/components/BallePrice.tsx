c80f27df4ddc7e9e12b4b12c4745351ebf1ce9ab
FranRom <maildefranz@gmail.com>
2021-05-25 23:48:13 +0200

    feat: Add chart url to balle price

--- src/widgets/Menu/components/BallePrice.tsx
+++ src/widgets/Menu/components/BallePrice.tsx
@@ -23,9 +23,12 @@ const PriceLink = styled.a`
   }
 `;
 
+const BALLEV_2_CONTRACT = '0x9714C04b34E6300964161c3aC37b86451E79152d';
+const CHART_URL = `https://charts.bogged.finance/?token=${BALLEV_2_CONTRACT}`;
+
 const BallePrice: React.FC<Props> = ({ ballePriceUsd }) => {
   return ballePriceUsd ? (
-    <PriceLink href="https://bscscan.com/address/0x9714C04b34E6300964161c3aC37b86451E79152d" target="_blank">
+    <PriceLink href={CHART_URL} target="_blank">
       <LogoRoundIcon width="24px" mr="8px" />
       <Text color="textSubtle" bold>{`$${ballePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
